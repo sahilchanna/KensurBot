562f6d59ef1295eb8f560593806f862b7972aaf8
sahilchanna <78895649+sahilchanna@users.noreply.github.com>
2021-03-04 18:53:24 +0530

    Update mention.py

--- userbot/modules/mention.py
+++ userbot/modules/mention.py
@@ -61,7 +61,7 @@ async def mention(event):
 CMD_HELP.update(
     {
         "mention": "Mention users with a custom name."
-        "\nUsage: `Hi @ender1324[bluid boi]`"
-        "\nResult: Hi [bluid boi](tg://resolve?domain=ender1324)"
+        "\nUsage: `no Hi if you don't reply me back @ender1324[bluid boi]`"
+        "\nResult: Hi now reply me[bluid boi](tg://resolve?domain=ender1324)"
     }
 )
