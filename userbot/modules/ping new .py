b08393a1aabf9a31d90e14ec391d76217d65efca
sahilchanna <78895649+sahilchanna@users.noreply.github.com>
2021-03-09 18:26:20 +0530

    Update ping new .py

--- userbot/modules/ping new .py
+++ userbot/modules/ping new .py
@@ -16,5 +16,5 @@ async def _(event):
     ms = (end - start).microseconds / 1000
     await asst.send_message(
         event.chat_id,
-        f"**pingy!!**\n `{ms}ms`",
+        f"**pong🎯!!**\n `{ms}ms`",
     )
